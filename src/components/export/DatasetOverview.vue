c0c8361492869fd0ffe6d332c47cecf69166dd06
sebastian-raubach <sebastian.raubach@hutton.ac.uk>
2020-10-01 16:40:07 +0100

    - CHG: Updated the look of the dataset overview widget at the top of export pages.

--- src/components/export/DatasetOverview.vue
+++ src/components/export/DatasetOverview.vue
@@ -1,17 +1,21 @@
 <template>
   <b-row v-if="datasets && datasets.length > 0">
     <!-- For each dataset -->
-    <b-col cols=12 sm=4 xl=2 v-for="(dataset, index) in datasets" :key="'datasets-overview-' + index">
+    <b-col cols=12 sm=4 xl=2 v-for="(dataset, index) in datasets" :key="`datasets-overview-${index}`">
       <b-card no-body>
-        <b-card-body>
+        <b-card-header>
           <!-- Truncated name with tooltip -->
-          <b-card-title title-tag="h5" v-b-tooltip.hover="dataset.datasetName">{{ dataset.datasetId + ' - ' + dataset.datasetName | truncateAfterWords(10) }}</b-card-title>
+          <h6 class="mb-0"  v-b-tooltip.hover="dataset.datasetName">{{ dataset.datasetId + ' - ' + dataset.datasetName | truncateAfterWords(10) }}</h6>
+        </b-card-header>
+        <b-card-body v-if="dataset.datasetDescription">
           <!-- Truncated description with tooltip -->
-          <b-card-sub-title class="dataset-card" v-if="dataset.datasetDescription" v-b-tooltip.hover="dataset.datasetDescription">{{ dataset.datasetDescription | truncateAfterWords(20) }}</b-card-sub-title>
-          <div v-if="dataset.locations">
-            <span v-for="country in getCountries(dataset.locations)" :key="`country-flag-${country}`" class="table-country text-nowrap" v-b-tooltip.hover :title="getCountryName(country)"><i :class="'flag-icon flag-icon-' + country.toLowerCase()" v-if="country"/></span>
-          </div>
+          <b-card-text class="dataset-card" v-b-tooltip.hover="dataset.datasetDescription">{{ dataset.datasetDescription | truncateAfterWords(20) }}</b-card-text>
         </b-card-body>
+        <b-card-footer v-if="dataset.locations">
+          <div>
+            <span v-for="country in getCountries(dataset.locations)" :key="`country-flag-${country}`" class="table-country text-nowrap mr-2" v-b-tooltip.hover :title="getCountryName(country)"><i :class="'flag-icon flag-icon-' + country.toLowerCase()" v-if="country"/></span>
+          </div>
+        </b-card-footer>
       </b-card>
     </b-col>
   </b-row>
