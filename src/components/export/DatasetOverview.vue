bb31892f02485ad98e0f6cbbd9ba5ea8df8a47bc
sebastian-raubach <sebastian.raubach@hutton.ac.uk>
2021-03-11 13:04:42 +0000

    - CHG: Extracted dataset summary into separate widget. - FIX: Fixed allele frequency license check.

--- src/components/export/DatasetOverview.vue
+++ src/components/export/DatasetOverview.vue
@@ -5,21 +5,7 @@
       <b-row v-if="datasets && datasets.length > 0">
         <!-- For each dataset -->
         <b-col cols=12 sm=4 xl=2 v-for="(dataset, index) in datasets" :key="`datasets-overview-${index}`">
-          <b-card no-body>
-            <b-card-header>
-              <!-- Truncated name with tooltip -->
-              <h6 class="mb-0"  v-b-tooltip.hover="dataset.datasetName">{{ dataset.datasetId + ' - ' + dataset.datasetName | truncateAfterWords(10) }}</h6>
-            </b-card-header>
-            <b-card-body v-if="dataset.datasetDescription">
-              <!-- Truncated description with tooltip -->
-              <b-card-text class="dataset-card" v-b-tooltip.hover="dataset.datasetDescription">{{ dataset.datasetDescription | truncateAfterWords(20) }}</b-card-text>
-            </b-card-body>
-            <b-card-footer v-if="dataset.locations">
-              <div>
-                <span v-for="country in getCountries(dataset.locations)" :key="`country-flag-${country}`" class="table-country text-nowrap mr-2" v-b-tooltip.hover :title="getCountryName(country)"><i :class="'flag-icon flag-icon-' + country.toLowerCase()" v-if="country"/></span>
-              </div>
-            </b-card-footer>
-          </b-card>
+          <IndividualDatasetWidget :dataset="dataset" />
         </b-col>
       </b-row>
     </b-collapse>
@@ -27,36 +13,20 @@
 </template>
 
 <script>
-const countries = require('i18n-iso-countries')
-countries.registerLocale(require('i18n-iso-countries/langs/en.json'))
+import IndividualDatasetWidget from '@/components/export/IndividualDatasetWidget'
 
 export default {
+  components: {
+    IndividualDatasetWidget
+  },
   props: {
     datasets: {
       type: Array,
       default: () => []
-    }
-  },
-  methods: {
-    getCountries: function (locations) {
-      if (locations) {
-        return [...new Set(locations.map(l => l.countryName))]
-      } else {
-        return []
-      }
-    },
-    getCountryName: function (code2) {
-      return countries.getName(code2, 'en')
     }
   }
 }
 </script>
 
 <style>
-.dataset-card {
-  overflow: hidden;
-  display: -webkit-box;
-  -webkit-line-clamp: 3;
-  -webkit-box-orient: vertical;
-}
 </style>
---
+++ src/components/export/IndividualDatasetWidget.vue
@@ -0,0 +1,75 @@
+<template>
+  <b-card no-body v-if="dataset">
+    <b-card-header>
+      <!-- Truncated name with tooltip -->
+      <h6 class="mb-0"  v-b-tooltip.hover="dataset.datasetName">{{ dataset.datasetId + ' - ' + dataset.datasetName | truncateAfterWords(10) }}</h6>
+    </b-card-header>
+    <b-card-body v-if="dataset.datasetDescription">
+      <!-- Truncated description with tooltip -->
+      <b-card-text class="dataset-card" v-b-tooltip.hover="collapsed ? dataset.datasetDescription : ''">
+        <template v-if="getDescriptionLength(dataset.datasetDescription) > lengthLimit && collapsed">
+          {{ dataset.datasetDescription | truncateAfterWords(lengthLimit) }}
+        </template>
+        <template v-else>
+          {{ dataset.datasetDescription }}
+        </template>
+      </b-card-text>
+      <b-button @click="collapsed = !collapsed" v-if="getDescriptionLength(dataset.datasetDescription) > lengthLimit">
+        {{ collapsed ? $t('buttonReadMore') : $t('buttonReadLess') }}
+      </b-button>
+    </b-card-body>
+    <b-card-footer v-if="dataset.locations">
+      <div>
+        <span v-for="country in countries" :key="`country-flag-${country}`" class="table-country text-nowrap mr-2" v-b-tooltip.hover :title="getCountryName(country)"><i :class="'flag-icon flag-icon-' + country.toLowerCase()" v-if="country"/></span>
+      </div>
+    </b-card-footer>
+  </b-card>
+</template>
+
+<script>
+const countries = require('i18n-iso-countries')
+countries.registerLocale(require('i18n-iso-countries/langs/en.json'))
+
+export default {
+  props: {
+    dataset: {
+      type: Object,
+      default: () => null
+    },
+    lengthLimit: {
+      type: Number,
+      default: 20
+    }
+  },
+  data: function () {
+    return {
+      collapsed: true
+    }
+  },
+  computed: {
+    countries: function () {
+      if (this.dataset && this.dataset.locations) {
+        return [...new Set(this.dataset.locations.map(l => l.countryName))]
+      } else {
+        return []
+      }
+    }
+  },
+  methods: {
+    getCountryName: function (code2) {
+      return countries.getName(code2, 'en')
+    },
+    getDescriptionLength: function () {
+      if (!this.dataset || !this.dataset.datasetDescription) {
+        return 0
+      }
+
+      return this.dataset.datasetDescription.split(' ').length
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
