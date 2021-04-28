59b8e27889ccb1ff9df90a8fbb52eba96091ed4e
sebastian-raubach <sebastian.raubach@hutton.ac.uk>
2021-04-28 16:29:41 +0100

    - CHG: Version bump.

--- src/containers/DefaultContainer.vue
+++ src/containers/DefaultContainer.vue
@@ -70,7 +70,7 @@
     </div>
     <TheFooter class="bg-dark">
       <div class="text-muted">
-        Version 4.1.3
+        Version 4.1.5
       </div>
       <div class="ml-auto">
         <a href="https://ics.hutton.ac.uk/get-germinate">Germinate</a>
